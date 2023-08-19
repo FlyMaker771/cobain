e53c372ec3de7d1e992a36eebcfac0d21c763803
unknown <rachardianbagaskoro@gmail.com>
2023-08-19 19:53:27 +0700

    first commit

---
+++ src/aplikasi_laundry/Aplikasi_Laundry.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi_laundry;
+
+/**
+ *
+ * @author Rachardian
+ */
+public class Aplikasi_Laundry {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login l = new login();
+        l.setVisible(true);
+
+//        koneksi k = new koneksi();
+//        k.connect();
+    }
+    
+}
